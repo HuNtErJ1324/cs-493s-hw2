e033186be832d646eea897cdde9079141b426cba
Johan Lindqvist <163306738+jdfld@users.noreply.github.com>
2025-06-10 22:12:30 -0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader
 import json
 
 
-def __main__():
+def __main__(args: list = None):
     # Initial setup of parameters
     parser = argparse.ArgumentParser(
         description=(
@@ -28,11 +28,6 @@ def __main__():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "--use_mod_dataset",
-        action="store_true",
-        help="whether to use the existing modulo dataset in the submitted code",
-    )
-    parser.add_argument(
         "--seq_len",
         type=int,
         default=-1,
@@ -42,25 +37,28 @@ def __main__():
         "--num_samples", type=int, default=16, help="the number of samples to infer"
     )
     parser.add_argument(
-        "--seed_file", type=str, default=None, help="full path to pretrained model file"
+        "--data_path", type=str, default="part2/data_test.csv", help="path to data for seeding"
     )
     parser.add_argument(
         "--out_file",
         type=str,
         default=None,
-        help="full path to output file where inference will be stored",
+        help="full path to output file where inference will be stored, if none no file is created",
+    )
+    parser.add_argument(
+        "--deterministic",
+        action="store_true",
+        help="Whether to use argmax or random sampling for inference",
     )
     parser.add_argument(
         "--print_samples",
-        type=bool,
-        default=True,
+        action="store_false",
         help="whether to also print samples in addition to storing in out_file",
     )
+    parser.add_argument("--model_name", type=str, default="mod_gpt", help="location of the model that performs the inference and its configs")
+    infer_configs = parser.parse_args(args)
 
-    parser.add_argument("--exp_name", type=str, default="mod_gpt", help="name of the experiment")
-    infer_configs = parser.parse_args()
-
-    with open(infer_configs.exp_name + "_config.json", "r") as f:
+    with open(infer_configs.model_name + "_config.json", "r") as f:
         config = Namespace(**json.load(f))
     print("Config loaded")
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -75,35 +73,50 @@ def __main__():
 
     samples = torch.zeros(
         infer_configs.num_samples, infer_configs.seq_len, dtype=torch.long
-    ).to(device)
-    if infer_configs.seed_file:
-        # TODO
-
-        # should provide co<mpatability for more advanced seeding of sampling
-        pass
-    else:
+    )
+    if infer_configs.data_path: # read seed data from file, expects same format as the mod files
+        test = pd.read_csv(infer_configs.data_path)
+        convert_equation_to_str(test)
+        test_questions, _ = tokenizer.tokenize(test["q_str"])
+        samples = torch.from_numpy(test_questions[:infer_configs.num_samples])
+        samples_infer = np.zeros(infer_configs.num_samples) + 2
+    else: # Random sampling, seeds the sequence with one random char from the vocab and then produces a sequence
         init_len = 1
-        for i in range(infer_configs.num_samples):
-            samples[i, 0] = tokenizer.tokenizer[
-                np.random.choice(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-            ]
+        samples[:, 0] = torch.Tensor(np.random.choice(tokenizer.vocab_size,size=infer_configs.num_samples))
+        samples_infer = np.zeros(infer_configs.num_samples) + infer_configs.seq_len-init_len
     # Create the transformer
     gpt_config = build_gpt_config(config)
     gpt = GPT(gpt_config)
     gpt.to(device)
-    gpt.load_state_dict(torch.load(config.exp_name + ".pth", map_location=device))
+    gpt.load_state_dict(torch.load(infer_configs.model_name + ".pth", map_location=device))
     gpt.eval()
+    samples = samples.to(device)
     init_len = 1
     if infer_configs.seq_len == -1:
         infer_configs.seq_len = config.block_size
     with torch.no_grad():
         for i in range(init_len, infer_configs.seq_len - 1):
-            samples[:, i] = gpt(samples).argmax(dim=2)[:, min(i, config.block_size - 1)]
+            logits = gpt(samples) 
+            logits = logits[:, min(i, config.block_size - 1), :]
+            probs = torch.softmax(logits, dim=-1) 
+            for j in range(infer_configs.num_samples):
+                p = probs[j].cpu().numpy()
+                if samples[j,i] == tokenizer.tokenizer['<pad>'] and samples_infer[j] > 0:
+                    samples_infer[j] -= 1
+                    if infer_configs.deterministic:
+                        samples[j,i] = p.argmax()
+                    else:
+                        samples[j, i] = np.random.choice(a=tokenizer.vocab_size, p=p)
     samples = samples.cpu().numpy().astype(int).astype(str)
     samples_as_text = tokenizer.untokenize(samples)
     if infer_configs.print_samples:
         for i in range(infer_configs.num_samples - 1):
             print(samples_as_text[i])
+            
+    if infer_configs.out_file:
+        with open(infer_configs.out_file, "w") as file:
+            file.writelines(samples_as_text)
+
 
 
 if __name__ == "__main__":
