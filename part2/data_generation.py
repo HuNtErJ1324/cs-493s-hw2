c1dcc181d8551004c74d1c827e90151d6cc8026e
Yeh Chan Yoo <13773473+yehchanyoo@users.noreply.github.com>
2025-06-11 18:47:41 -0700

    Reverted data generation code and output to older correct version

--- part2/data_generation.py
+++ part2/data_generation.py
@@ -10,72 +10,48 @@ from sklearn.model_selection import train_test_split
 # - o: Operator (addition (+), subtraction (-), or division (/))
 
 # Step 1: Create numpy arrays, each containing values for a column
-rng = np.random.default_rng(101)
-base_97 = np.repeat(rng.integers(100_000, size=98), 98)
-base_113 = np.repeat(rng.integers(100_000, size=114), 114)
-p = np.concatenate((np.repeat(97, 98 * 98 * 3), np.repeat(113, 114 * 114 * 3)))
-a = np.concatenate(
-    (
-        np.tile(base_97, 3),
-        np.tile(base_113, 3),
-    )
-)
-b = np.concatenate((np.tile(np.arange(98), 98 * 3), np.tile(np.arange(114), 114 * 3)))
-o = np.concatenate(
-    (
-        np.repeat("+", 98 * 98),
-        np.repeat("-", 98 * 98),
-        np.repeat("/", 98 * 98),
-        np.repeat("+", 114 * 114),
-        np.repeat("-", 114 * 114),
-        np.repeat("/", 114 * 114),
-    )
-)
+p = np.concatenate((np.repeat(97, 98*98*3), np.repeat(113, 114*114*3)))
+a = np.concatenate((np.tile(np.repeat(np.arange(98), 98), 3), np.tile(np.repeat(np.arange(114), 114), 3)))
+b = np.concatenate((np.tile(np.arange(98), 98*3), np.tile(np.arange(114), 114*3)))
+o = np.concatenate((np.repeat("+", 98*98), np.repeat("-", 98*98), np.repeat("/", 98*98), 
+                   np.repeat("+", 114*114), np.repeat("-", 114*114), np.repeat("/", 114*114)))
 
 # Step 2: Find c using NumPy operations
 c = np.array([])
 for p_val in (97, 113):
     # Templates
-    if p_val == 97:
-        a_template = base_97
-    elif p_val == 113:
-        a_template = base_113
-    else:
-        raise NotImplementedError()
-
-    b_template = np.tile(np.arange(p_val + 1), p_val + 1)
+    a_template = np.repeat(np.arange(p_val+1), p_val+1)
+    b_template = np.tile(np.arange(p_val+1), p_val+1)
 
     # Addition with mod
     c_addition = np.mod(np.add(a_template, b_template), p_val)
-
+ 
     # Subtraction with mod
     c_subtraction = np.mod(np.subtract(a_template, b_template), p_val)
 
     # Division with mod
     c_division = np.mod(np.floor_divide(a_template, b_template), p_val)
-
+    
     # Put resulting values in c
     c = np.concatenate((c, c_addition, c_subtraction, c_division))
-
+    
 
 # Step 4: Put them into one Pandas DataFrame and publish as csv file
 # with the columns converted to characters for use with the Transformers model
 # & removing rows with division by zero
-data = pd.DataFrame(
-    {
-        "p": p.astype(str),
-        "o": o.astype(str),
-        "a": a.astype(str),
-        "b": b.astype(str),
-        "c": c.astype(int).astype(str),
-    }
-).query("not (o == '/' and b == '0')")
-data.to_csv("data.csv", index=False)
+data = pd.DataFrame({
+    "p": p.astype(str),
+    "o": o.astype(str),
+    "a": a.astype(str),
+    "b": b.astype(str),
+    "c": c.astype(int).astype(str)
+}).query("not (o == '/' and b == '0')")
+data.to_csv('data.csv', index=False)
 
 # Step 5: Create train, test, and validation splits
 # (50% train, 25% test, 25% validation)
 data_train, data_test = train_test_split(data, test_size=0.5)
 data_test, data_val = train_test_split(data_test, test_size=0.5)
-data_train.to_csv("data_train.csv", index=False)
-data_test.to_csv("data_test.csv", index=False)
-data_val.to_csv("data_val.csv", index=False)
+data_train.to_csv('data_train.csv', index=False)
+data_test.to_csv('data_test.csv', index=False)
+data_val.to_csv('data_val.csv', index=False)
